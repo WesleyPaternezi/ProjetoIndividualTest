722d0e96ddf13a03ad11fd00fdd0f7607d348ecb
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2021-05-30 14:34:07 -0300

    arrumando script

--- API-projeto-site/config/paterneziSQL.sql
+++ API-projeto-site/config/paterneziSQL.sql
@@ -3,14 +3,6 @@ create database enviaDados;
 
 use enviaDados;
 
-CREATE TABLE leitura (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	temperatura DECIMAL,
-	umidade DECIMAL,
-	momento DATETIME,
-	fkcaminhao INT
-);
-
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
