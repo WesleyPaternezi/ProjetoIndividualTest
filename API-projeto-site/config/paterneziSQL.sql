0d5e01c31ca41d880c98922910cb922f28d14c78
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2021-05-30 13:59:47 -0300

    salvando scriptMYSQL

---
+++ API-projeto-site/config/paterneziSQL.sql
@@ -0,0 +1,25 @@
+/* para workbench - local - desenvolvimento */
+create database enviaDados;
+
+use enviaDados;
+
+CREATE TABLE leitura (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	temperatura DECIMAL,
+	umidade DECIMAL,
+	momento DATETIME,
+	fkcaminhao INT
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	login VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE publicacao (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(100),
+	fkUsuario INT
+);
